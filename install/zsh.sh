dbd222e776bae34cd87f0a9644f6d4f009e73335
Philippe Clement <clementp67@gmail.com>
2020-08-13 19:45:02 +0200

    Always install oh my zsh

--- install/zsh.sh
+++ install/zsh.sh
@@ -2,11 +2,7 @@
 
 if ! command_exists zsh; then
     echo "zsh not found. Installing..."
-    if [ "$(uname)" == "Darwin" ]; then 
-        bash -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-    else
-      sudo apt install zsh
-    fi
+    bash -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 elif ! [[ $SHELL =~ .*zsh.* ]]; then
     echo "Configuring zsh as default shell"
     chsh -s $(which zsh)
