1b31f97d1d94ae07d91d8e8cc3a960d0bd0fe6b7
Philippe Clement <philippe.clement@zalando.de>
2023-06-16 17:38:45 +0200

    Update config

--- install/zsh.sh
+++ install/zsh.sh
@@ -2,8 +2,9 @@
 
 if ! command_exists zsh; then
     echo "zsh not found. Installing..."
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+
 elif ! [[ $SHELL =~ .*zsh.* ]]; then
     echo "Configuring zsh as default shell"
     chsh -s $(which zsh)
